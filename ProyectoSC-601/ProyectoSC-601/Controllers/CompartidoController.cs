e287199ee5ce1a28ee813de33e200860af8b1e9e
nacho13moya03 <ignaciomoya2003@gmail.com>
2023-11-14 19:57:50 -0600

    Arreglos

---
+++ ProyectoSC-601/ProyectoSC-601/Controllers/CompartidoController.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoSC_601.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSC_601.Controllers
+{
+    public class CompartidoController : Controller
+    {
+        public ActionResult Acerca()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ProyectoSC-601/ProyectoSC-601/Models/CarritoModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using ProyectoSC_601.Entities;
+using System.Configuration;
+using System.EnterpriseServices.Internal;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web.Mvc;
+
+namespace ProyectoSC_601.Models
+{
+    public class CarritoModel
+    {
+        public string rutaServidor = ConfigurationManager.AppSettings["RutaApi"];
+
+        public string RegistroCliente(ClienteEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "carrito";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+
+    }
+}
