db75dd3ae57025cb551e951ae5201652b4bd4611
MichNB <86860984+MichNB@users.noreply.github.com>
2023-11-14 18:11:09 -0600

    Inventario

---
+++ APIProyectoSC-601/APIProyectoSC-601/Entities/InventarioEnt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIProyectoSC_601.Entities
+{
+    public class InventarioEnt
+    {
+        public int ID_Inventario { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Style { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
