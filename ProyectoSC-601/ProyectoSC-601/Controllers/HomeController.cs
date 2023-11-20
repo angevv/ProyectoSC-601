9d6021687aa2335d416d82d4cbc8a7dc9c487b1e
angevv <69274747+angevv@users.noreply.github.com>
2023-11-20 16:47:27 -0600

    Parte de Contáctenos Lista

--- ProyectoSC-601/ProyectoSC-601/Controllers/HomeController.cs
+++ ProyectoSC-601/ProyectoSC-601/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoSC_601.Models;
+﻿using ProyectoSC_601.Entities;
+using ProyectoSC_601.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,31 @@ namespace ProyectoSC_601.Controllers
 {
     public class HomeController : Controller
     {
+
+        ContactoModel modelContacto = new ContactoModel();
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        //Se llama al modelo para enviar la informacion de la seccion contactenos
+        [HttpPost]
+        public ActionResult Index(InfoContacto entidad)
+        {
+            string respuesta = modelContacto.EnviarInformacion(entidad);
+
+            if (respuesta == "OK")
+            {
+                ViewBag.MensajeExitoso = "La información se ha enviado con éxito";
+                return View();
+            }
+            else
+            {
+                ViewBag.MensajeNoExitoso = "No se ha podido enviar la informacion";
+                return View();
+            }
         }
 
         public ActionResult IndexAdmin() 
