fec88a4310c74051f6bff3f1d3c59447f974caa3
angevv <69274747+angevv@users.noreply.github.com>
2023-11-23 14:53:13 -0600

    Interfaces Inventario

---
+++ ProyectoSC-601/ProyectoSC-601/Controllers/InventarioController.cs
@@ -0,0 +1,41 @@
+﻿using ProyectoSC_601.Entities;
+using ProyectoSC_601.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace ProyectoSC_601.Controllers
+{
+    public class InventarioController : Controller
+    {
+        
+        InventarioModel modelInventario=new InventarioModel();
+
+
+        /* Consulta todos los productos registrados en el sistema */
+        [HttpGet]
+        public ActionResult ConsultaInventario()
+        {
+            //var datos = modelInventario.ConsultarInventario();
+            //return View(datos);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegistrarProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ModificarProducto()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ProyectoSC-601/ProyectoSC-601/Models/InventarioModel.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using ProyectoSC_601.Entities;
+using System.Collections.Generic;
+using System.Configuration;
+using System.EnterpriseServices.Internal;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSC_601.Models
+{
+    public class InventarioModel
+    {
+        //Se hace referencia a la ruta del servidor configurada en Web.config
+        public string rutaServidor = ConfigurationManager.AppSettings["RutaApi"];
+
+        //Funcion para consultar todos los clientes por parte del administrador
+        public List<InventarioEnt> ConsultarInventario()
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "ConsultarInventario";
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<List<InventarioEnt>>().Result;
+            }
+        }
+
+    }
+}
