3b63f387cb31eda83343faaace1dd622688398c3
angevv <69274747+angevv@users.noreply.github.com>
2023-12-05 18:40:30 -0600

    Arreglos Catalogo

---
+++ ProyectoSC-601/ProyectoSC-601/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using ProyectoSC_601.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSC_601.Controllers
+{
+    public class ProductoController : Controller
+    {
+        InventarioModel modelInventario = new InventarioModel();
+
+        [HttpGet]
+        public ActionResult Catalogo()
+        {
+            ViewBag.TotalInventario = modelInventario.ContarTotalInventario();
+            var datos = modelInventario.ConsultarInventario();
+            return View(datos);
+        }
+
+    }
+}
