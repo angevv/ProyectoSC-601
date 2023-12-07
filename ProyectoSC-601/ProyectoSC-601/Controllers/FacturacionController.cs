29038d2965ba4f88dd2b55144dae161fe7e607e4
angevv <69274747+angevv@users.noreply.github.com>
2023-12-06 21:44:03 -0600

    Arreglos

---
+++ ProyectoSC-601/ProyectoSC-601/Controllers/FacturacionController.cs
@@ -0,0 +1,40 @@
+﻿using ProyectoSC_601.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSC_601.Controllers
+{
+    public class FacturacionController : Controller
+    {
+        [HttpGet]
+        public ActionResult Facturacion()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult FacturaDetalle()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult FacturacionCliente()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult FacturaDetalleCliente()
+        {
+            return View();
+
+        }
+    }
+}
